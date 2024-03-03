45a36bf257ec4494853f6c498f3b708ad0201a5d
Vania <vanakocmar952@gmail.com>
2024-03-03 10:28:51 +0200

    first

---
+++ main.py
@@ -0,0 +1,23 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import*
+import sys
+
+def evt_btn_cliced():
+    win.setWindowTitle(len.text())
+
+app = QApplication(sys.argv)
+
+win = QDialog()
+win.setWindowTitle('LOL')
+win.setGeometry(800,200,500,500)
+
+len = QLineEdit('Defolt text',win)
+len.move(180,190)
+
+btn = QPushButton('Ubdate Window Title',win)
+btn.move(170,240)
+btn.resize(150,50)
+btn.clicked.connect(evt_btn_cliced)
+
+win.show()
+sys.exit(app.exec_())
