8d5278c47715cdabf49546f28fe70a4d76c1b336
Vania <vanakocmar952@gmail.com>
2024-03-16 19:41:33 +0200

    leson3_*

--- 2.py
+++ 2.py
@@ -1,34 +1,56 @@
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel,QVBoxLayout,QPushButton
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel,QVBoxLayout,QRadioButton,QHBoxLayout,QMessageBox
 from random import randint
-def winers():
-    a = randint(0,9)
-    b = randint(0,9)
-    if a == b:
-        winer.setText(str(a))
-        winer2.setText(str(b))
-        text.setText('Ви виграли! Зіграйте знову')
-    else:
-        winer.setText(str(a))
-        winer2.setText(str(b))
-        text.setText('Ви програли! Зіграйте знову')
+
+def show_lose():
+    ms = QMessageBox()
+    ms.setText('Пощастить іншим разом!')
+    ms.exec_()
+def show_win():
+    ms = QMessageBox()
+    ms.setText('Ви виграли зустріч з творцями каналу!')
+    ms.exec_()
+
 app = QApplication([])
 win = QWidget()
-text = QLabel('Натисни щоби взяти участь')
-v1 = QVBoxLayout()
-buttn = QPushButton('Випробувати удачу')
-winer = QLabel('?')
-winer2 = QLabel('?')
-win.resize(500,300)
-win.setWindowTitle('Лотерея')
+win.setWindowTitle('Конкурс від Crazy People')
+win.resize(400,200)
+l_qves = QLabel('Як звали першого ютуб-блогера, який набрав 100000000 підписників')
+r_bt_3 =   QRadioButton('SlivkiShow')
+r_bt_2 = QRadioButton('Рет і Лінк')
+r_bt_1 = QRadioButton('PewDiePie')
+r_bt_4 = QRadioButton('TheBrianMaps')
+r_bt_5 = QRadioButton('Mister Max')
+r_bt_6 = QRadioButton('EeOneGuy')
 
-v1.addWidget(text, alignment =Qt.AlignCenter)
-v1.addWidget(winer, alignment=Qt.AlignCenter)
-v1.addWidget(winer2, alignment=Qt.AlignCenter)
-v1.addWidget(buttn, alignment=Qt.AlignCenter)
-buttn.clicked.connect(winers)
-win.setLayout(v1)
+v_main = QVBoxLayout()
+h_1 =QHBoxLayout()
+h_2 = QHBoxLayout()
+h_3 = QHBoxLayout()
+h_4 = QHBoxLayout()
 
+h_3.addWidget(l_qves, alignment= Qt.AlignCenter)
+h_1.addWidget(r_bt_1, alignment= Qt.AlignCenter)
+h_1.addWidget(r_bt_2, alignment= Qt.AlignCenter)
+h_2.addWidget(r_bt_3, alignment= Qt.AlignCenter)
+h_2.addWidget(r_bt_4, alignment= Qt.AlignCenter)
+h_4.addWidget(r_bt_5, alignment= Qt.AlignCenter)
+h_4.addWidget(r_bt_6, alignment= Qt.AlignCenter)
+
+v_main.addLayout(h_3)
+v_main.addLayout(h_1)
+v_main.addLayout(h_2)
+v_main.addLayout(h_4)
+
+
+r_bt_1.clicked.connect(show_win)
+r_bt_3.clicked.connect(show_lose)
+r_bt_2.clicked.connect(show_lose)
+r_bt_4.clicked.connect(show_lose)
+r_bt_5.clicked.connect(show_lose)
+r_bt_6.clicked.connect(show_lose)
+
+win.setLayout(v_main)
 
 win.show()
 app.exec_()
